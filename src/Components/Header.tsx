48f8ccf579fc06882c33de35e560fdbc566d4597
kirkade <mookj@mail.ru>
2022-09-15 12:45:41 +0300

    create first components and style

---
+++ src/Components/Header.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Header = () => {
+    return (<header className='header'>
+            <img src={'https://adamlevin.com/wp-content/uploads/2020/06/joomla-logo-png-transparent.png'}></img>
+        </header>
+    )
+}
---
+++ src/Components/Profile.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src={'https://quick-trips.com/wp-content/uploads/2017/12/4444444444444445.jpg'}></img>
+            </div>
+            <div>
+                ava+descript
+            </div>
+            <div>
+                my posts
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    )
+}
